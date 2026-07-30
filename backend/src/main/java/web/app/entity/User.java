ca6922af8f27bd5b4cbcd74e0a4e3c06c75022d3
DarshParekh205 <parekhdarsh002@gmail.com>
2026-07-30 08:54:53 +0530

    Removed Intellij project files

--- backend/src/main/java/web/app/entity/User.java
+++ backend/src/main/java/web/app/entity/User.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
@@ -69,4 +70,8 @@ public class User implements UserDetails {
     private List<Account> accounts;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(SimpleGrantedAuthority::new).toList();
+    }
 }
